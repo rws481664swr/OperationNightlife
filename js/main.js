cdbc30b40010f1b0e3cfd0f52273a3c69aa12307
Ari Kalfus <arikalfus@gmail.com>
2014-09-15 13:30:58 -0400

    Basic webpage with background

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Authors: Ari Kalfus, Burak Sezer, Sam Raphael, Wesley Wei Qian
+
+var model = {
+	title: "Codename: Operation Nightlife"
+};
+
+var nightlifeApp = angular.module("nightlifeApp", []) //TODO: module name might want to be changed.
+
+// Controller for map view
+.controller("mapCtrl", function($scope, $rootScope) {
+	$scope.nightlife = model;
+});
