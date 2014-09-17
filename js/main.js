b80796f5110c137595aec2386908f25f9b34a5b6
Ari Kalfus <arikalfus@gmail.com>
2014-09-17 11:10:01 -0400

    Read the book

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 // Authors: Ari Kalfus, Burak Sezer, Sam Raphael, Wesley Wei Qian
 
 var model = {
-	title: "Codename: Operation Nightlife"
+	
 };
-
+// Fluid API Implementation - if you do not know what this means, READ THE BOOK
 var nightlifeApp = angular.module("nightlifeApp", []) //TODO: module name might want to be changed.
 
-.controller("headerCtrl", function($scope, $rootScope) {
-	$scope.nightlife = model;
+// highest level scope
+.controller("rootCtrl", function($scope, $rootScope) {
+	$scope.title = "Codename: Operation Nightlife";
 })
 
 // Controller for map view
