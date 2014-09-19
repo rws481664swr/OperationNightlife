92eb438c6b472969e6e9d9c752c8b91a44001cb1
Ari Kalfus <arikalfus@gmail.com>
2014-09-19 15:44:56 -0400

    Current location marker on map

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 // Authors: Ari Kalfus, Burak Sezer, Sam Raphael, Wesley Wei Qian
 
 var model = {
-	
+	position: {lat: 42.3677816, lng: -71.2585826, zoom: 17}
 };
 // Fluid API Implementation - if you do not know what this means, READ THE BOOK
 var nightlifeApp = angular.module("nightlifeApp", []) //TODO: module name might want to be changed.
 
+.run(function() {
+	var mapOptions = {
+		center: {lat: model.position.lat, lng: model.position.lng},
+		zoom: model.position.zoom
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	var marker = new google.maps.Marker({
+		position: model.position,
+		map: map,
+		title: "Your Current Location"
+	});
+})
+
 // highest level scope
 .controller("rootCtrl", function($scope, $rootScope) {
-	$scope.title = "Codename: Operation Nightlife";
+	$scope.title = "Hermes: Protector of The Road";
+	$scope.model = model;
 })
 
 // Controller for map view
 .controller("mapCtrl", function($scope, $rootScope) {
-	$scope.nightlife = model;
+	$scope.model = model;
 });
