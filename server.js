15617239b1d8f3b754d3f44c882fa1e33baf849f
Ari Kalfus <arikalfus@gmail.com>
2014-10-13 11:58:00 -0400

    Testing server function

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.put('/model/:users/:id', function(req, res) {
     var users = db.get(req.params.users);
     users.insert({
         "_id": req.params.id,
-        "position": req.params.data
+        "position": req.data
     });
     res.json(200, {});
 });
